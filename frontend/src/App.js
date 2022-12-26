1b26c9919e2988872be2d40fdbc29f69206469d9
g-loubna <kl_guelmani@esi.dz>
2022-12-26 23:20:17 +0100

    change landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import logo from './FindIn.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="App">  
+        <img src={logo} className="App-logo"  alt="logo" />
+
+          
+            
+           <p> FindIn UN site de soutien scolaire,</p>
+           <p> ou vous pouvez déposer vos annonces ,</p>
+           <p>chercher l’annonce que vous plait ou le prof</p>
+           <p> qui peut vous aider , juste une clique...  </p>
+          
+
     </div>
   );
 }
